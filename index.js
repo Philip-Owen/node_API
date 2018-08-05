9aec3d6fbfe7402114cddc639837e6458dae3709
Philip-Owen <31570473+Philip-Owen@users.noreply.github.com>
2018-08-05 17:35:15 -0500

    added parsing of headers and payloads

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 // Dependencies
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 const server = http.createServer((req,res) => {
 
@@ -20,11 +21,26 @@ const server = http.createServer((req,res) => {
   // Get HTTP Method
   const method = req.method.toLowerCase();
 
-  // Send res
-  res.end('Hello World\n');
+  // Get headers as an object
+  const headers = req.headers;
 
-  // Log req path
-  console.log(`Request recieved on path:'${trimmedPath}' with method:'${method}' and with these query string params:`, queryStringObject);
+  // Get payload if any
+  const decoder = new StringDecoder('utf-8');
+  let buffer = '';
+  req.on('data', data => {
+    
+    buffer += decoder.write(data);
+  });
+  req.on('end', () => {
+
+    buffer += decoder.end();
+
+    // Send res
+    res.end('Hello World\n');
+
+    // Log headers
+    console.log(`Request recieved with this payload: `, buffer);
+  });
 
 });
 
